e2b788412061167788e01bb17456fcc20edc0119
chandankrchoubey <chandan.choubey@di2solutions.com>
2021-02-22 00:06:21 +0530

    sensible mobiles

---
+++ app/src/main/java/com/chandan/coroutinesexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.chandan.coroutinesexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        runBlocking {
+          val mJob: Deferred<String> = async{
+                  "chandan"
+            }
+            mJob.join()
+        }
+    }
+}
